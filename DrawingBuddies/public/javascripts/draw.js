0a7e4eff8881c96aa080c78cfe68d93d6b896e46
Jessica Li <jli93@cs.washington.edu>
2015-11-21 02:20:11 -0800

    get color/size/tool from other clients
    send color/size/tool info to server and broadcast to other clients

--- DrawingBuddies/public/javascripts/draw.js
+++ DrawingBuddies/public/javascripts/draw.js
@@ -1,13 +1,42 @@
 var myPath;
 var otherPaths = {};
 
-var color = 'black';
-var size = '5';
-var tool = 'pen';
+var myColor = 'black';
+var mySize = '5';
+var myTool = 'pen';
+
+window.onload = function () {
+    document.getElementById("color").onclick = selectColor;
+    document.getElementById("size").onclick = selectSize;
+    document.getElementById("tool").onclick = selectTool;
+};
+
+// on click listener for color
+function selectColor() {
+    var color = document.getElementById("color");
+    myColor = color.options[color.selectedIndex].value;
+}
+
+// on click listener for size
+function selectSize() {
+    var size = document.getElementById("size");
+    mySize = size.options[size.selectedIndex].value;
+}
+
+// on click listener for tool
+function selectTool() {
+    // if tool is  eraser, then change color to white
+    var tool = document.getElementById("tool");
+    myTool = tool.options[tool.selectTool].value;
+    if (myTool == 'eraser') {
+        myColor = 'white';
+    }
+}
 
 function onMouseDown(event) {
 	myPath = new Path();
-	myPath.strokeColor = 'black';
+	myPath.strokeColor = myColor;
+    myPath.strokeWidth = mySize;
 }
 
 function onMouseDrag(event) {
@@ -20,11 +49,15 @@ function onMouseUp(event) {
 	endPath();
 }
 
-function initPath(clientnum){
+// called for when other clients draw on the board and they
+// don't have a path.
+function initPath(clientnum, clientColor, clientSize){
 	p = new Path();
-	p.strokeColor = 'black';
+	myPath.strokeColor = clientColor;
+    myPath.strokeWidth = clientSize;
 	otherPaths[clientnum] = p;
 }
+
 // This function sends the data for a circle to the server
 // so that the server can broadcast it to every other user
 function emitPath( point ) {
@@ -34,7 +67,9 @@ function emitPath( point ) {
     // An object to describe the circle's draw data
     var data = {
         x: point.x,
-        y: point.y
+        y: point.y,
+        color: myColor,
+        size: mySize
     };
 
     // send a 'drawCircle' event with data and sessionId to the server
@@ -50,20 +85,20 @@ function endPath(){
 	io.emit('endPath', io.io.engine.id);
 }
 
-function drawPath(x, y, clientnum){
+function drawPath(x, y, clientnum, clientColor, clientSize){
 	if (!otherPaths[clientnum]){
-		initPath(clientnum);
+		initPath(clientnum, clientColor, clientSize);
 	}
 	var p = new Point(x,y);
 	otherPaths[clientnum].add(p);
 	view.draw();
 }
 
-io.on( 'drawPath', function( data , clientnum) {
+io.on( 'drawPath', function( data , clientnum ) {
 	console.log("received:")
     console.log( data.x );
     console.log( data.y );
-    drawPath(data.x, data.y, clientnum);
+    drawPath(data.x, data.y, clientnum, data.color, data.size);
 });
 
 io.on( 'endPath', function( session ){
