9a577296e494efb46258b48933cad1c5eadd8b30
MackenzieM <kenzie93@cs.uw.edu>
2015-11-20 21:32:52 -0800

    added a mouse

---
+++ DrawingBuddies/public/javascripts/draw.js
@@ -0,0 +1,14 @@
+var myPath;
+
+function onMouseDown(event) {
+	myPath = new Path();
+	myPath.strokeColor = 'black';
+}
+
+function onMouseDrag(event) {
+	myPath.add(event.point);
+}
+
+function onMouseUp(event) {
+
+}
