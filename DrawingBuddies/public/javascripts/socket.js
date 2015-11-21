5a5b118a4ff049e322ad3a77747e521e110bdaec
Jessica Li <jli93@cs.washington.edu>
2015-11-20 20:54:56 -0800

    fixed socket.io to send ping/pong
    browser sends ping, server responds with pong

--- DrawingBuddies/public/javascripts/socket.js
+++ DrawingBuddies/public/javascripts/socket.js
@@ -1,2 +1,12 @@
 // Connect to the Node.js Server
-io = io.connect('/');
+io = io();
+
+// (1): send a ping event with some data to the Server
+console.log("socket: browser says ping (1)")
+io.emit('ping', { some: 'data'});
+
+// (4): When the browser receives a pong event
+// console log a message and the events data
+io.on('pong', function (data) {
+    console.log( 'socket: browser receives pong (4)', data );
+});
