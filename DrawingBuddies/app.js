c2ecce93fbb932775b85ad67be83d1a3a0ff3957
Jessica Li <jli93@cs.washington.edu>
2015-11-20 20:20:56 -0800

    fixed app.js

--- DrawingBuddies/app.js
+++ DrawingBuddies/app.js
@@ -1,38 +1,40 @@
-var express = require('express');
-var path = require('path');
+// 1. Express requires these dependencies
+var express = require('express')
+  , routes = require('./routes')
+  , users = require('./routes/users')
+  , http = require('http')
+  , path = require('path');
+
+
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var cookieParser = require('cookie-parser');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// 2. Configure our application
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  // app.use(express.logger('dev'));
+  // app.use(express.bodyParser());
+  // app.use(express.methodOverride());
+  // app.use(app.router);
+  // app.use(express.static(path.join(__dirname, 'public')));
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// 3. Configure error handling
+  // app.use(express.errorHandler());
 
-// error handlers
+
+// // error handlers
 
 // development error handler
 // will print stacktrace
@@ -57,4 +59,78 @@ app.use(function(err, req, res, next) {
 });
 
 
-module.exports = app;
+// 4. Setup Routes
+// app.get('/', routes.index);
+// app.get('/users', user.list);
+
+app.use('/', routes);
+app.use('/users', users);
+
+// 5. Enable Socket.io
+var server = http.createServer(app).listen( app.get('port') );
+var io = require('socket.io').listen(server, function() {
+  console.log("Express server listening on port " + app.get('port'));
+});
+
+
+// // express requires these dependencies
+// var express = require('express');
+// var path = require('path');
+// var favicon = require('serve-favicon');
+// var logger = require('morgan');
+// var cookieParser = require('cookie-parser');
+// var bodyParser = require('body-parser');
+
+// var routes = require('./routes/index');
+// var users = require('./routes/users');
+
+// var app = express();
+
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+
+// // uncomment after placing your favicon in /public
+// //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(logger('dev'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', routes);
+// app.use('/users', users);
+
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+
+// // error handlers
+
+// // development error handler
+// // will print stacktrace
+// if (app.get('env') === 'development') {
+//   app.use(function(err, req, res, next) {
+//     res.status(err.status || 500);
+//     res.render('error', {
+//       message: err.message,
+//       error: err
+//     });
+//   });
+// }
+
+// // production error handler
+// // no stacktraces leaked to user
+// app.use(function(err, req, res, next) {
+//   res.status(err.status || 500);
+//   res.render('error', {
+//     message: err.message,
+//     error: {}
+//   });
+// });
+
+
+// module.exports = app;
