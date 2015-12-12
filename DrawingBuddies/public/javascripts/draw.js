ac53710bb17e4baa06c2688b4c0ab9097d42aa8c
MackenzieM <kenzie93@cs.uw.edu>
2015-12-11 20:14:59 -0800

    simulation progress

--- DrawingBuddies/public/javascripts/draw.js
+++ DrawingBuddies/public/javascripts/draw.js
@@ -218,7 +218,7 @@ function drawSingleShape(shape, centerX, centerY, radius, color) {
     return shapePath;
 }
 
-///botss stuff
+///BOT SIMULATION BEGINS
 function click(x,y){
     var ev = document.createEvent("MouseEvent");
     var el = document.elementFromPoint(x,y);
@@ -232,10 +232,6 @@ function click(x,y){
     );
     el.dispatchEvent(ev);
 }
-function simulateDown(x, y) {
-    var el = document.elementFromPoint(x, y);
-    $(el).mousedown();
-}
 function simulateClick(x, y) {
     var ev = { 
         point: {
@@ -246,27 +242,27 @@ function simulateClick(x, y) {
     onMouseDownHelper(ev);
     onMouseUpHelper(ev);
 }
-function simulateUp(x, y) {
-    var el = document.elementFromPoint(x, y);
-    $(el).trigger("mouseup");
+function simulateDown(x, y) {
+    var ev = { 
+        point: {
+            x: x,
+            y: y
+        }
+    };
+    onMouseDownHelper(ev);
 }
 function simulateDrag(x, y) {
     var el = document.elementFromPoint(x, y);
     jQuery(el).mousemove();
 }
-function simulateDraw(x,y){
-    console.log("drawing " + x + " " + y + " ");
-    var ev = document.createEvent("MouseEvent");
-    var el = document.elementFromPoint(x,y);
-    ev.initMouseEvent(
-        "click",
-        true /* bubble */, true /* cancelable */,
-        window, null,
-        x, y, 0, 0, /* coordinates */
-        false, false, false, false, /* modifier keys */
-        0 /*left*/, null
-    );
-    el.dispatchEvent(ev);
+function simulateUp(x,y){
+    var ev = { 
+        point: {
+            x: x,
+            y: y
+        }
+    };
+    onMouseUpHelper(ev);
 }
 ///bots stuff ends
 
